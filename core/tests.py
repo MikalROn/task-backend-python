dd64b9e189ca6df7e509230dc41ea0cd82a17c96
Daniel <heromon.9010@gmail.com>
2024-12-08 08:12:42 -0300

    add: mais testes

--- core/tests.py
+++ core/tests.py
@@ -9,35 +9,56 @@ class TaskTest(APITestCase):
     def setUp(self):
         self.task = Task.objects.create(descricao="test0", completo=False)
         self.task1 = Task.objects.create(descricao="test1", completo=False)
-        self.url = 'api/' + 'task'
+        self.task2 = Task.objects.create(descricao="test2", completo=False)
+        
+        self.uri = '/api/task/'
     
-
-
-
     def test_task_create(self):
         data = {
             "descricao": "test"
         }
         
-        r = self.client.post(self.url, data)
+        r = self.client.post(self.uri, data)
         
-        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        self.assertEqual(r.status_code, status.HTTP_201_CREATED)
     
     
     def test_task_list(self):
         
-        r = self.client.get(self.url)
+        r = self.client.get(self.uri)
         
         self.assertEqual(r.status_code, status.HTTP_200_OK)
+    
     
     def test_task_update(self):
         data = {
             "descricao": "test"
         }
         
-        r = self.client.put(f'/api/task/{self.task.pk}', data)
+        r = self.client.put(f'{self.uri}{self.task.pk}/', data)
 
         print(r.status_code, r.content, r.context)
         
         self.assertEqual(r.status_code, status.HTTP_200_OK)
     
+    
+    def test_task_delete(self):
+        data = {
+            "descricao": "test"
+        }
+        
+        r = self.client.put(f'{self.uri}{self.task.pk}/', data)
+
+        print(r.status_code, r.content, r.context)
+        
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+    
+    
+    def test_task_get(self):
+        data = {
+            "descricao": "test Update"
+        }
+        
+        r = self.client.put(f'{self.uri}{self.task.pk}/', data)
+        
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
