dc84207f4c9a91cae115d4ee31c254dea416ceae
Daniel <heromon.9010@gmail.com>
2024-12-08 02:51:35 -0300

    add: test

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+import core.views as views
+
+from django.urls import include, path
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+
+router.register(r'task', views.TaskListViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+    
