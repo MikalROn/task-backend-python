73bb6cd8c81cd17bf4420b86ac8311daed84ab7f
Daniel <heromon.9010@gmail.com>
2024-12-09 03:12:03 -0300

    fix: url

--- taskBackEndPython/settings.py
+++ taskBackEndPython/settings.py
@@ -111,3 +111,61 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[{asctime}] {levelname} {name} ({module}:{lineno}) {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'django_debug.log',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        # Logs relacionados ao banco de dados
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file'],
+            'propagate': False,
+        },
+        # Logs relacionados às requisições HTTP
+        'django.request': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file'],
+            'propagate': False,
+        },
+        # Logs relacionados ao sistema Django em geral
+        'django': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file'],
+            'propagate': False,
+        },
+        # Logs de segurança
+        'django.security': {
+            'level': 'WARNING',
+            'handlers': ['console', 'file'],
+            'propagate': False,
+        },
+        # Adicione loggers personalizados conforme necessário
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['console', 'file'],
+    },
+}
--- taskBackEndPython/urls.py
+++ taskBackEndPython/urls.py
@@ -4,5 +4,6 @@ from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     path('api/', include('core.urls')),
+    #path('api', include('core.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
